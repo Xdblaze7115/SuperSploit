f1ab194d96aa30f5031d5b77446766d7b2ccbb79
Xdblaze7115 <160746019+Xdblaze7115@users.noreply.github.com>
2025-01-08 13:41:24 +0000

    Update mainsource.lua

--- mainsource.lua
+++ mainsource.lua
@@ -1,17 +1,31 @@
 if Game.PlaceId == 6000468131 then
-    local Players = game:GetService("Players")
+	local Players = game:GetService("Players")
 
 	_G.TagEsp = false
 	_G.LimbEsp = false
-	
+
 	local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Xdblaze7115/SuperSploit/refs/heads/main/source.lua", true))()
-	
+
 	local Window = Library:CreateLibrary("SuperSploit", "Granny")
-	
+
+	local Tab = Window:CreateTab("Player")
+
+	local Slider = Window:CreateSlider("WalkSpeed", 100, function(value)
+		if Players.LocalPlayer.Character then
+			Players.LocalPlayer.Character:WaitForChild("Humanoid").WalkSpeed = value
+		end
+	end)
+
+	local Slider = Window:CreateSlider("JumpPower", 100, function(value)
+		if Players.LocalPlayer.Character then
+			Players.LocalPlayer.Character:WaitForChild("Humanoid").JumpPower = value
+		end
+	end)
+
 	local Tab = Window:CreateTab("Visual")
-	
+
 	local Label = Window:CreateLabel("ESP")
-	
+
 	local Toggle1 = Window:CreateToggle("TagEsp", function(state)
 		if state then
 			_G.TagEsp = true
@@ -39,7 +53,7 @@ if Game.PlaceId == 6000468131 then
 			end
 		end
 	end)
-	
+
 	local Toggle2 = Window:CreateToggle("LimbEsp", function(state)
 		if state then
 			_G.LimbEsp = true
@@ -67,11 +81,11 @@ if Game.PlaceId == 6000468131 then
 			end
 		end
 	end)
-	
+
 	function AddTagEsp(plr)
 		if plr.Character then
 			local char = plr.Character
-	
+
 			local Esp = Instance.new("BillboardGui")
 			Esp.Parent = char:WaitForChild("Head")
 			Esp.Name = "Esp"
@@ -79,12 +93,12 @@ if Game.PlaceId == 6000468131 then
 			Esp.AlwaysOnTop = true
 			Esp.Size = UDim2.new(2, 0, 2, 0)
 			Esp.StudsOffset = Vector3.new(0, 2, 0)
-	
+
 			local Frame = Instance.new("Frame")
 			Frame.Parent = Esp
 			Frame.BackgroundTransparency = 1
 			Frame.Size = UDim2.new(1, 0, 1, 0)
-	
+
 			local Name = Instance.new("TextLabel")
 			Name.Parent = Frame
 			Name.Size = UDim2.new(1, 0, 1, 0)
@@ -96,7 +110,7 @@ if Game.PlaceId == 6000468131 then
 			Name.TextScaled = true
 		end
 	end
-	
+
 	function RemoveTagEsp(plr)
 		if plr.Character and plr.Character:FindFirstChild("Head") then
 			local head = plr.Character.Head
@@ -105,7 +119,7 @@ if Game.PlaceId == 6000468131 then
 			end
 		end
 	end
-	
+
 	function AddLimbEsp(plr)
 		if plr.Character then
 			local char = plr.Character
@@ -118,7 +132,7 @@ if Game.PlaceId == 6000468131 then
 						Esp.Face = face
 						Esp.AlwaysOnTop = true
 						Esp.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-	
+
 						local Box = Instance.new("Frame")
 						Box.Parent = Esp
 						Box.Size = UDim2.new(1, 0, 1, 0)
@@ -130,7 +144,7 @@ if Game.PlaceId == 6000468131 then
 			end
 		end
 	end
-	
+
 	function RemoveLimbEsp(plr)
 		if plr.Character then
 			for _, part in pairs(plr.Character:GetChildren()) do
