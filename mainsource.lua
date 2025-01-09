a508e029882af7c52fccd46289e7cac38c6f5a6c
Xdblaze7115 <160746019+Xdblaze7115@users.noreply.github.com>
2025-01-09 10:25:04 +0000

    Update mainsource.lua

--- mainsource.lua
+++ mainsource.lua
@@ -1,3 +1,3 @@
-if game:HttpGet("https://raw.githubusercontent.com/Xdblaze7115/scripts/refs/heads/main/".. [game.PlaceId] .. ".lua?token=GHSAT0AAAAAAC4YZICA3JAVH2W5ZH37SAD2Z37UNRQ", true) then
+if game:HttpGet("https://raw.githubusercontent.com/Xdblaze7115/scripts/refs/heads/main/" .. [game.PlaceId] .. ".lua?token=GHSAT0AAAAAAC4YZICA3JAVH2W5ZH37SAD2Z37UNRQ", true) then
 	local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Xdblaze7115/SuperSploit/refs/heads/main/source.lua", true))()
 end
